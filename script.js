cd75811b23ceaa7b6e1fd12240c532cabc4da5f5
KISHOR <kishorsuresh210@gmail.com>
2024-11-08 13:42:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ b.addEventListener("keydown",(event)=>{
                         <li id="item-${index}">
                             <input type='checkbox' id='checkbox${index}' onclick="check(${index})" class='c'>
                             <b id='b-${index}'class='display-5'>${text.value}</b>
+                            <button id='edit${index}' onclick='editer(${index})' class='btn btn-outline-danger'>edit</button>
                             <button id='delete${index}' onclick='deleter(${index})' class='btn btn-outline-primary'>delete</button>
                         </li>`;
                 text.value = '';
@@ -35,11 +36,50 @@ function abc() {
                 <li id="item-${index}">
                     <input type='checkbox' id='checkbox${index}' onclick="check(${index})" class='c'> 
                     <b id='b-${index}'class='display-5'>${text.value}</b>
+                    <button id='edit${index}' onclick='editer(${index})' class='btn btn-outline-danger'>edit</button>
                     <button id='delete${index}' onclick='deleter(${index})' class='btn btn-outline-primary'>delete</button>
                 </li>`;
         text.value = '';
         // console.log(x[x.length - 1]); 
     }
+}
+function editer(index){
+    const tex = document.getElementById(index);
+    const s = document.createElement('span');
+    let c  = document.getElementById(`b-${index}`);
+    const t = document.getElementById(`item-${index}`);
+    s.style.marginTop='20px';
+    s.style.width='100%';
+    s.style.height='auto';
+    s.style.backdropFilter='blur';
+    s.style.zIndex='2';
+    s.style.display='flex';
+    s.style.placeItems='center';
+    s.style.justifyContent='center';
+    s.style.flexWrap='wrap';
+    s.id=`span-${index}`
+    s.innerHTML +=`<input type='text' class='form-control' value='${c.innerHTML}' id='in-${index}' style='
+      margin:1.5px;
+      padding:2px;
+      width:80%;
+      height:auto;
+      min-width:50px;
+      font-size: xx-large;
+      font-weight: bolder;
+      border-radius: 10px;
+      animation-name: te;
+      animation-duration: 2s;
+      animation-timing-function: ease-in;' >
+      
+      <div style='display:flex;justify-content:center;margin-top:20px;' id='d-${index}'>
+      <button id='Cancel${index}' onclick='Cancel(${index})' class='btn btn-outline-danger'>Cancel</button>
+      <span style='height:1px;width:10px;'id='d2${index}'></span>
+      <button id='save${index}' onclick='save(${index})' class='btn btn-outline-primary' >Save</button></div>`; 
+    
+    document.body.appendChild(s);
+    t.appendChild(s);
+
+    //console.log(c.innerHTML);
 }
 function check(id){
     // console.log(id);
@@ -51,6 +91,7 @@ function check(id){
     }
      else {
         b.style.textDecoration = 'none'; // Reset line-through if unchecked
+        
     }
 }   
 }
@@ -66,4 +107,34 @@ function deleter(id) {
         // Remove the item from the to-do list
         item.style.display = 'none';
     } 
+}
+function Cancel(index){
+    let d = document.getElementById(`in-${index}`);
+    let e = document.getElementById(`Cancel${index}`);
+    let f = document.getElementById(`save${index}`);
+    let g = document.getElementById(`d-${index}`);
+    let h = document.getElementById(`d2${index}`);
+    let i=document.getElementById(`span-${index}`);
+    d.remove();
+    e.remove();
+    f.remove();
+    g.remove();
+    h.remove();
+    i.remove();
+}
+function save(index){
+    let d = document.getElementById(`in-${index}`);
+    let e = document.getElementById(`Cancel${index}`);
+    let f = document.getElementById(`save${index}`);
+    let g = document.getElementById(`d-${index}`);
+    let c  = document.getElementById(`b-${index}`);
+    let h = document.getElementById(`d2${index}`);
+    let i=document.getElementById(`span-${index}`);
+    c.innerHTML = d.value;
+    d.remove();
+    e.remove();
+    f.remove();
+    g.remove();
+    h.remove();
+    i.remove();
 }
